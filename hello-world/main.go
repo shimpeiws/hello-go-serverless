466117304e77bd4bd0fc2203bbed265f93b669af
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-04-14 22:57:54 +0900

    Add s3 client

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 var (
@@ -21,8 +27,14 @@ var (
 	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	resp, err := http.Get(DefaultHTTPGetAddress)
+	fmt.Println("Befoew ENV VARIABLES")
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
@@ -38,6 +50,32 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	if len(ip) == 0 {
 		return events.APIGatewayProxyResponse{}, ErrNoIP
+	}
+
+	accessKey := os.Getenv("AWS_ACCESS_KEY")
+	secretKey := os.Getenv("AWS_ACCESS_SECRET_KEY")
+	region := os.Getenv("AWS_REGION")
+
+	creds := credentials.NewStaticCredentials(accessKey, secretKey, "")
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(region),
+		Credentials: creds,
+	})
+
+	fmt.Println("Befoew New!!!")
+
+	// Create S3 service client
+	svc := s3.New(sess)
+	result, err := svc.ListBuckets(nil)
+	if err != nil {
+		exitErrorf("Unable to list buckets, %v", err)
+	}
+
+	fmt.Println("Buckets:")
+
+	for _, b := range result.Buckets {
+		fmt.Printf("* %s created on %s\n",
+			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
 	}
 
 	return events.APIGatewayProxyResponse{
