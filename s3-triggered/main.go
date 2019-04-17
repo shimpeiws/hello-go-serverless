7fdcd68b42c372ecad91820bf181b72d805afd53
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-04-17 17:26:15 +0900

    Update

--- s3-triggered/main.go
+++ s3-triggered/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -18,7 +19,6 @@ func createSession() *session.Session {
 	return session.Must(session.NewSession(&aws.Config{
 		S3ForcePathStyle: aws.Bool(true),
 		Region:           aws.String(os.Getenv("REGION")),
-		Endpoint:         aws.String(os.Getenv("S3_ENDPOINT")),
 	}))
 }
 
@@ -62,18 +62,25 @@ func download(bucket string, key string) (f *os.File, err error) {
 }
 
 func handler(ctx context.Context, req events.S3Event) error {
+	log.Print("Handler Executed!!!")
+
 	bucketName := req.Records[0].S3.Bucket.Name
 	key := req.Records[0].S3.Object.Key
+
+	log.Print("bucketName = " + bucketName)
+	log.Print("key = " + key)
 
 	file, err := download(bucketName, key)
 	if err != nil {
 		return errors.Wrap(err, "Error failed to s3 download")
 	}
+	log.Print("downloaded")
 
 	_, err = upload(file, key)
 	if err != nil {
 		return errors.Wrap(err, "Error failed to s3 upload")
 	}
+	log.Print("uploaded")
 
 	return nil
 }
